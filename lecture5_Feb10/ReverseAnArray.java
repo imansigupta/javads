0f422e65c2ae0f59510edb8da89fdd3a0e7b0187
LOKESH <32417590+LA1997@users.noreply.github.com>
2019-10-19 19:20:48 +0530

    Update ReverseAnArray.java

--- lecture5_Feb10/ReverseAnArray.java
+++ lecture5_Feb10/ReverseAnArray.java
@@ -2,16 +2,18 @@ package lecture5_Feb10;
 
 public class ReverseAnArray {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] arr = {10,20,30,40,50};
-		int x ;
+	public static void reverse(int[] arr) {
+		int x;
 		for(int i = 0;i<arr.length/2;i++ ){
 			x = arr[i];
 			arr[i] =  arr[arr.length-1-i];
 			arr[arr.length-1-i] = x;
-			
 		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {10,20,30,40,50};
+		reverse(arr);
 		for(int i = 0; i<arr.length;i++){
 			System.out.println("new array element at "+ i +"is "+arr[i]);
 		}
